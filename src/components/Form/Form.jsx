3be76fba2e8180bc8194cdf9ce7a33de00675a81
ssulphurr <ssulphurr@gmail.com>
2023-05-13 23:09:31 +0300

    adds files from previous homework

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import css from './Form.module.css';
+
+class Form extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  nameInputId = nanoid();
+  numberInputId = nanoid();
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit({ ...this.state });
+
+    this.reset();
+  };
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    return (
+      <form className={css.form} onSubmit={this.handleSubmit}>
+        <label className={css.label} htmlFor={this.nameInputId}>
+          Name
+        </label>
+
+        <input
+          className={css.input}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={this.state.name}
+          onChange={this.handleChange}
+          id={this.nameInputId}
+        />
+
+        <label className={css.label} htmlFor={this.numberInputId}>
+          Number
+        </label>
+        <input
+          className={css.input}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={this.state.number}
+          onChange={this.handleChange}
+          id={this.numberInputId}
+        />
+
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
+
+Form.propTypes = { onSubmit: PropTypes.func.isRequired };
